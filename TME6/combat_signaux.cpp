8beca55a1d4f7a175773270ef969c9a7380c4340
hakim ogal <hakim.ogal@etu.sorbonne-universite.fr>
2025-11-07 12:38:13 +0100

    07 11 2025

---
+++ TME6/combat_signaux.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+pid_t pid;
+volatile sigatomic_t PV = 3;
+
+void attaque(pid_t adversaire){
+struct sigaction sa;
+    sa.sa_handler = attacque_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if(sigaction(SIGUSR1,&sa,nullptr) <0){
+        perror("sigaction");
+        exit(1);
+    }
+    if (kill(adversaire,SIGUSR1))
+    rsleep();
+}
+void defense(){
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if(sigaction(SIGUSR1,&sa,nullptr) <0){
+        perror("sigaction");
+        exit(1);
+    }
+    rsleep();
+}
+
+void combat(pid_t adversaire){
+
+}
+
+int main(arg c, char **argv){
+    int T = atoi (argv[1]);
+    
+    //handler_ALARM_je le remplis apres
+    
+    
+    pid =fork();
+
+    if(pid < 0){perror("fork"); return 2;}
+    else if (pid == 0) { 
+        //Luke
+
+    } else {
+        //Vador
+
+    }
+
+}
